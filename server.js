250f462733cc5d27c4e9a7fedeef9fbe57d84cbf
Magdy Elmansy <magdyelmansy4@gmail.com>
2018-12-24 01:18:41 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('veiw engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append file');
+    }
+  });
+  console.log(log);
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  //res.send('<h1>Hello Express</h1>');
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMsg: 'Welcome To Home Page'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    status: 'error handling request'
+  })
+});
+app.listen(3000, () => {
+  console.log('server is up on port 3000');
+});
